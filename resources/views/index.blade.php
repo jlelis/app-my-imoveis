3a2a28fe8e8012042c04d678089be8dfd09b3354
jlelis <jerfao@hotmail.com>
2021-07-20 11:51:28 -0300

    ajuste no mudar slide com os botões

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,63 +7,87 @@
 
             <div class="row">
                 <div class="col s12 m4 l4">
-
+                    <!--Card Inicial -->
                     <div class="card hoverable">
-                        <div class="card  large">
+                        <div class="card">
                             <div class="card-image ">
                                 <div class="carousel carousel-slider" data-indicators="true">
+                                    <div class="carousel-fixed-item center middle-indicator">
+                                        <div class="left">
+                                            <a href="Previo"
+                                                class="movePrevCarousel middle-indicator-text waves-effect waves-light content-indicator"><i
+                                                    class="material-icons left  middle-indicator-text">chevron_left</i></a>
+                                        </div>
 
+                                        <div class="right">
+                                            <a href="Siguiente"
+                                                class=" moveNextCarousel middle-indicator-text waves-effect waves-light content-indicator"><i
+                                                    class="material-icons right middle-indicator-text">chevron_right</i></a>
+                                        </div>
+                                    </div>
                                     @forelse($imovel->imovelFotos as $foto)
 
-                                        <a class="carousel-item" href="{{route('imoveis.show',$imovel->id)}}">
-                                            <img src="{{$foto->path_images}}" class="responsive-img" style="width: 100%; height: 100%;">
+                                        <a class="carousel-item" href="{{ route('imoveis.show', $imovel->id) }}">
+                                            <img src="{{ $foto->path_images }}" class="responsive-img"
+                                                style="width: 100%; height: 100%;">
                                         </a>
 
                                     @empty
                                     @endforelse
                                 </div>
-                                <span class="card-title"><strong>{{$imovel->finalidade->nome}}</strong></span>
+                                <span class="card-title"><strong>{{ $imovel->finalidade->nome }}</strong></span>
                             </div>
 
                             <div class="card-content">
-                                <p class="big"><strong>Valor R$:</strong>{{number_format($imovel->preco, 2, ',', '.')}}
+                                <p class="big"><strong>Valor R$:</strong>{{ number_format($imovel->preco, 2, ',', '.') }}
                                 </p>
-                                <p class="big"><strong>Cidade: </strong>{{$imovel->cidade->nome}}</p>
-                                <p class="big"><strong>Bairro: </strong> {{$imovel->endereco->bairro}}</p>
-                                <p class="big"><strong>Rua: </strong> {{$imovel->endereco->rua}}</p>
+                                <p class="big"><strong>Cidade: </strong>{{ $imovel->cidade->nome }}</p>
+                                <p class="big"><strong>Bairro: </strong> {{ $imovel->endereco->bairro }}</p>
+                                <p class="big"><strong>Rua: </strong> {{ $imovel->endereco->rua }}</p>
 
 
                             </div>
                             <div class="card-action center">
-                                <a href="{{ route('imoveis.show',$imovel->id) }}"
-                                   class="waves-effect waves-light btn-small">Mais Detalhes</a>
+                                <a href="{{ route('imoveis.show', $imovel->id) }}"
+                                    class="waves-effect waves-light btn-small">Mais Detalhes</a>
                             </div>
                         </div>
                     </div>
                     <br>
                 </div>
-                @empty
+            @empty
 
-                @endforelse
-            </div>
-            <div class="fixed-action-btn">
-                <a class="btn-floating btn-large waves-effect waves-light" href="{{route('imoveis.create')}}">
-                    <i class="large material-icons">add</i>
-                </a>
-            </div>
+        @endforelse
+        </div>
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large waves-effect waves-light" href="{{ route('imoveis.create') }}">
+                <i class="large material-icons">add</i>
+            </a>
+        </div>
 
 
     </section>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
 
             $('.carousel.carousel-slider').carousel({
                 fullWidth: true,
-                indicators: true
+                indicators: false
             });
-            setInterval(function () {
+
+            // move next carousel
+            $('.moveNextCarousel').click(function(e) {
+                e.preventDefault();
+                e.stopPropagation();
                 $('.carousel').carousel('next');
-            }, 5000);
+            });
+
+            // move prev carousel
+            $('.movePrevCarousel').click(function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                $('.carousel').carousel('prev');
+            });
         });
     </script>
 
